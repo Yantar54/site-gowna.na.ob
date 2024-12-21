39c602d7cb7a800f8abc8a8552c97a7c49ef90c5
Yantar54 <83553754yantar45@gmail.com>
2024-12-21 07:07:53 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,74 @@
+// const personeF = {
+//   name: "eblan"
+//   , age: 15
+//   , status: "dayn"
+//   ,
+
+// };
+
+// function changer(){
+//   personeF.age = 4;
+
+// };
+// changer();
+// console.log(personeF);
+
+// function eblanChanger(neEblan){
+//   neEblan.name = "ymni";
+//   neEblan.status = "krutoi";
+//   neEblan.age = 25;
+//  };
+// eblanChanger(personeF);
+
+// console.log(personeF);
+
+
+// function inFunction(){
+//   console.log('hi baby');
+// };
+
+// function initialaseFu(innerFu){
+//   innerFu();
+// };
+
+// initialaseFu(inFunction);
+// function colorChanger(spanItem){
+//   spanItem.style.color = 'red';
+// };
+
+// const fatherObj = {
+//   a: 'text'
+//   , b: 'text'
+//   , c: 'text'
+//   ,
+
+// };
+
+// const nextObj = {
+//   ...fatherObj
+//   , new: 'new'
+//   ,
+
+// };
+
+// const last = (item, time = Date()) =>({
+//   ...item
+//   , time
+// });
+
+// console.log(last(nextObj));
+
+
+const ment = () => {
+  throw new Error('stope');
+
+};
+
+
+try{
+  ment();
+}catch (error){
+  console.error('ti lox');
+}
+
+// 5150
